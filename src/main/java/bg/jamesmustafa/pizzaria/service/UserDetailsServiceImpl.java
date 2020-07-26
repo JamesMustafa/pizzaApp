5888207305df7608b99551ab1e95c80283cd1feb
Djem Mustafa <djemko123@gmail.com>
2020-07-26 22:13:52 +0300

    authentication started

---
+++ src/main/java/bg/jamesmustafa/pizzaria/service/UserDetailsServiceImpl.java
@@ -0,0 +1,55 @@
+package bg.jamesmustafa.pizzaria.service;
+
+import bg.jamesmustafa.pizzaria.dto.UserDTO;
+import bg.jamesmustafa.pizzaria.entity.User;
+import bg.jamesmustafa.pizzaria.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    //private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new UserDTO(user);
+    }
+
+    private org.springframework.security.core.userdetails.User map(User user){
+        List<GrantedAuthority> authorities = user.
+                getRoles().
+                stream().
+                map(r -> new SimpleGrantedAuthority(r.getAuthority())).
+                collect(Collectors.toList());
+
+        org.springframework.security.core.userdetails.User result =
+                new org.springframework.security.core.userdetails.User(
+                        user.getUsername(),
+                        user.getPassword(),
+                        authorities
+                );
+
+        return result;
+    }
+}
