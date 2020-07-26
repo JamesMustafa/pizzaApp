14ce0f431e508ab2595ccb210cdcb2e5111fb0c6
Djem Mustafa <djemko123@gmail.com>
2020-07-26 19:55:33 +0300

    Initial commit

---
+++ src/test/java/bg/jamesmustafa/pizzaria/PizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.jamesmustafa.pizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
